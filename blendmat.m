f77e5f27beca4e6f18917f9717ad7cb6890c8301
tgoulet <tgoulet@purdue.edu>
2025-03-14 23:51:51 -0400

    Changes: -Changed .blend file name, cleaned up and commented code -Added looping mechanism and termination command in MATLAB so that Blender will continue rendering images until MATLAB commands it to shut down the socket server -Added status messages passed from Blender to MATLAB to help users know when images are rendered and where they are located

--- blendmat.m
+++ blendmat.m
@@ -1,16 +1,45 @@
-% Define the vector to send
-vector = [5.45, 0, 0, 90, 0, 90];
-% Convert the vector to a string
-vectorStr = sprintf('%.5f,%.5f,%.5f,%.5f,%.5f,%.5f', vector);
-% Set up the socket client
-t = tcpclient('localhost', 9997);
-% Send the vector to Blender
-write(t, vectorStr);
-% Close the connection
+%% Blender Command Script
+clear; clc; close all
+
+%set up the socket server client (number must match Blender script)
+t = tcpclient('localhost', 9998);
+
+%define the pose vector(s) to send to Blender (one pose vector/row)
+pose = [5.45, 0, 0, 90, 0, 90; ...
+    5.45, 0, 0, 100, 10, 100];
+
+%loop through vectors and send to Blender
+for i = 1:size(pose, 1)
+
+    %convert pose vector to character array, send to Blender
+    poseStr = sprintf('%.6f,%.6f,%.6f,%.6f,%.6f,%.6f', pose(i, :));
+    write(t, poseStr);
+
+    %wait for response from Blender and print message to command window
+    print_message(t)
+
+end
+
+%send command for Blender to shut-down socket server
+write(t, 'terminate');
+print_message(t)
+
+%close the socket server
 clear t;
 
-%picks up image and shows it
-keyboard;
-image_path = "C:/Users/Adam/Documents/render.png";
-img = imread(image_path);
-imshow(img);
+
+function print_message(t)
+
+while t.NumBytesAvailable == 0
+    pause(0.1);
+end
+message = read(t);
+fprintf(char(message))
+
+end
+
+% %picks up image and shows it
+% keyboard;
+% image_path = "C:\tmp\render_0.png";
+% img = imread(image_path);
+% imshow(img);
